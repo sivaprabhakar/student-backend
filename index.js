781ced31de4eda25a68036cd39f159bf5799dc14
sivaprabhakar <sivaprabhakaran.94@gmail.com>
2024-09-19 12:54:03 +0530

    student-backend

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ dotenv.config()
 const PORT = process.env.PORT
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: '*', 
+  }));
 app.use(express.json())
 
 app.use('/', studentRoutes);
